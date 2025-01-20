79cb0280fea3141212cdd191fef8dac6c1a1677f
KartikeyJoshi916 <95592393+KartikeyJoshi916@users.noreply.github.com>
2025-01-20 20:30:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,10 +1,3 @@
-//your JS code here. If required.
-function print() {
-	let loadContainer=document.getElementById("loadDiv");
-	loadContainer.innerHTML+=`
-	<p>DOM load success</p>
-	`;
-}
 document.addEventListener("DOMContentLoaded", (event) => {
-  print();
+  console.log("DOM load success");
 });
